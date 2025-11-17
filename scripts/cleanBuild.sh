74a1bad602476bcafe38189f9ac628ce50bceddd
Pedro Molina <pedro.molinaespinosa@colaborador.elcorteingles.es>
2025-11-17 23:25:06 +0100

    Modificada estructura de carpetas.

--- scripts/cleanBuild.sh
+++ scripts/cleanBuild.sh
@@ -3,5 +3,8 @@ flutter clean
 find . -name "*.g.dart" -delete
 find . -name "*.freezed.dart" -delete
 flutter pub get
+cd ios
+pod install 
+cd ..
 flutter pub run build_runner build --delete-conflicting-outputs
 
